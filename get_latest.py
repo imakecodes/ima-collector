210c15d7be4787e68750c98643cb025a84d7d15c
Michel Wilhelm <michelwilhelm@gmail.com>
2020-11-25 21:27:31 -0300

    Adding first real and updated data

--- get_latest.py
+++ get_latest.py
@@ -1,12 +1,19 @@
 from http import HTTPStatus
 import json
+import os
 
 import requests
-import pedulum
+import pendulum
 
 
 resp = requests.get("https://balneabilidade.ima.sc.gov.br/relatorio/mapa")
 if resp.status_code == HTTPStatus.OK:
     with open("latest.json", "w+") as f:
-        f.write(json.dumps(resp.json), indent=4))
+        f.write(json.dumps(resp.json(), indent=4))
+
+        filename = "{}.json".format(pendulum.now().strftime("%Y-%m-%d"))
+        os.system(f"cp latest.json ./data/{filename}") 
+
+
+
 
